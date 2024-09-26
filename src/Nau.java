c592b047c5e06dc86ce876b6178683d673183200
yas <yaser.benktib@mataro.epiaedu.cat>
2024-09-26 20:43:29 +0200

    Creat el arxiu de Main i personatges

---
+++ src/Nau.java
@@ -0,0 +1,13 @@
+public class Nau {
+    public static void main(String[] args) throws Exception {
+        Nau e = new Nau();
+        e.principal();
+    }
+
+    public void principal(){
+
+        
+
+    }
+
+}
