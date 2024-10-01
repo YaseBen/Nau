0f80974e1b9d5681bb5fe515b406ac86195a2ffc
yas <yaser.benktib@mataro.epiaedu.cat>
2024-10-01 21:07:13 +0200

    final de classe

--- src/Nau.java
+++ src/Nau.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Nau {
+    Random rand = new Random();
+
     public static void main(String[] args) {
         Nau e = new Nau();
         e.principal();
@@ -9,8 +11,28 @@ public class Nau {
 
     public void principal() {
 
-        ArrayList<habitaciones> mapa = new ArrayList<>(11);
-        Random random = new Random();
+        ArrayList<Habitaciones> mapa = new ArrayList<>(10);
+        // Mostrem la posició inicial
+        mapa.mostrarPosicioActual();
+        // Movent-nos cap a la dreta i esquerra
+        mapa.moureDreta(); // Et mous a la dreta
+        mapa.moureDreta(); // Et mous a la dreta
+        mapa.moureEsquerra(); // Et mous a l'esquerra
+        mapa.moureEsquerra(); // Et mous a l'esquerra
+        mapa.moureEsquerra(); // Et mous a l'esquerra
+
+        mapa.add();
+
+        String[][] mapa = new String[3][6];
+        int o;
+        int p;
+        boolean eina = false;
+        while (eina == false) {
+            o = rand.nextInt(3);
+            p = rand.nextInt(6);
+            mapa[o][p] = "?";
+            eina = true;
+        }
         boolean jugar = true;
 
         while (!jugar) {
@@ -18,5 +40,4 @@ public class Nau {
         }
 
     }
-
 }
--- src/habitaciones.java
+++ src/habitaciones.java
@@ -1,6 +1,16 @@
+import java.util.Random;
+
+public class habitaciones {
+    boolean eina = true;
+    boolean donuts = true;
+    boolean vestitEspacial = true;
+    Random rand = new Random();
+    int num = rand.nextInt(0, 6);
+    Objectes Objecte[] = new Objectes[num];
 
 
-public class habitaciones{
-  
-
+    
 }
+
+
+
