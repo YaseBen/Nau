ddef5638f2f071ef77df5fb9b5b4a4308227fd4b
yas <yaser.benktib@mataro.epiaedu.cat>
2024-10-01 19:56:08 +0200

    Creat el bucle

--- src/Nau.java
+++ src/Nau.java
@@ -1,12 +1,16 @@
 public class Nau {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Nau e = new Nau();
         e.principal();
     }
 
     public void principal(){
 
-        
+        boolean jugar = true;
+
+        while (!jugar) {
+            
+        }
 
     }
 
