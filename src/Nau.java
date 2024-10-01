eecd8f5a2d9300c5e124e6ec32fc5dc20556ea8f
yas <yaser.benktib@mataro.epiaedu.cat>
2024-10-01 20:26:01 +0200

    Mapa creat

--- src/Nau.java
+++ src/Nau.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Nau {
     public static void main(String[] args) {
         Nau e = new Nau();
@@ -6,6 +8,8 @@ public class Nau {
 
     public void principal(){
 
+        ArrayList<habitaciones> mapa= new ArrayList<>(11);
+        
         boolean jugar = true;
 
         while (!jugar) {
